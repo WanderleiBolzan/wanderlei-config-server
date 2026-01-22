ba1500c591a89dc236d266b1e83e66f700f83c3c
wanderlei jose bolzan <wbolzanjp@gmail.com>
2026-01-22 10:14:54 -0300

    Initial commit: Config Server setup ms exchange

---
+++ src/main/java/br/com/wanderlei/ExchengeServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.wanderlei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchengeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchengeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/wanderlei/ExchengeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wanderlei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchengeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
